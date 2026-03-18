25bb072af1d18f276c7ef787a21a01ed1e30770b
dilja2001 <diljadaser@gmail.com>
2026-03-18 19:03:16 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const app = express()
 
 app.use(cors({
   origin: ["https://crmclient-umber.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
 app.use(express.json())
